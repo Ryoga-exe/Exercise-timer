4cb3b0f764295f0f808e8d0482a5f07b0aa439a5
Ryoga.exe <49950206+Ryoga-exe@users.noreply.github.com>
2020-06-20 17:33:07 +0900

    test commit

--- main.js
+++ main.js
@@ -5,10 +5,30 @@ function playbeep(isHigh=false) {
     isHigh ? soundH.play() : soundL.play();
 }
 let count = 0;
+let times = 0;
+let num = 0;
+
+let counter = 0;
+let isRunning = false;
 window.addEventListener("DOMContentLoaded", function() {
     document.getElementById("start").onclick = function() {
-        playbeep(count%5==0);
-        count++;
-        document.getElementById("circled").classList.toggle("animation1");
+        if (!isRunning){
+            times = document.getElementById("inputTimes").value;
+            num = document.getElementById("inputNumber").value;
+            isRunning = true;
+            counter = 0;
+            foofoo();
+        }
     }
 })
+
+function foofoo() {
+    let hoge = (count % times == 0);
+    playbeep(hoge);
+    count++;
+    document.getElementById("circled").classList.toggle("animation1");
+    if (hoge) counter++;
+    if (counter < num)
+        window.setTimeout("foofoo()", 600);
+    else isRunning = false;
+}
